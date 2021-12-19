81a8908e3bcc7722f40aab283c731af9663f00fe
n0b0dykn0wsme <95046817+n0b0dykn0wsme@users.noreply.github.com>
2021-12-19 11:59:22 -0800

    Added file made in visual studio

---
+++ Untitled-1.py
@@ -0,0 +1,5 @@
+operation = input("Enter Math Operation")
+numone = input("Enter 1st number") 
+numtwo = input("Enter 2nd number")
+answr = numone + operation + numtwo
+print ("Your answer is str(answr)")
